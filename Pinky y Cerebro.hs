13451d1f33f24a0040304cfd6dce308a9f7427a1
darkonis13 <deko_gagli@hotmail.com>
2018-07-26 13:33:13 -0300

    Punto 4 casi completo y type Reporte de punto 5

--- Pinky y Cerebro.hs
+++ Pinky y Cerebro.hs
@@ -14,28 +14,53 @@ inteligenciaSuperior incrementoDeCoeficiente unAnimal = unAnimal{coeficienteInte
 pinkificar :: Transformacion
 pinkificar unAnimal = unAnimal{capacidades = []}
 
-superPoderes :: Transformacion
-superPoderes unAnimal
- | (especie unAnimal) == "Elefante" = unAnimal {capacidades = (capacidades unAnimal) ++ ["no tenerle miedo a los ratones"]}
- | (especie unAnimal) == "Raton" && (coeficienteIntelectual unAnimal) > 100 = unAnimal{capacidades = (capacidades unAnimal) ++ ["Hablar"]}
+superpoderes::Transformacion
+superpoderes unAnimal
+  | (especie unAnimal) == "Elefante" = capacitar "No tenerle miedo a los ratones" unAnimal
+  | (especie unAnimal) == "Raton" && compararCoeficiente 100 unAnimal = capacitar "Hablar" unAnimal
+
+
+compararCoeficiente:: Int->CriterioDeExito
+compararCoeficiente coeficienteASuperar unAnimal = (coeficienteIntelectual unAnimal) > coeficienteASuperar
+
+capacitar::String->Transformacion
+capacitar capacidad unAnimal = unAnimal {capacidades = (capacidades unAnimal) ++ [capacidad]}
 
 --PUNTO 3
 type CriterioDeExito = Animal->Bool
+antropomorfico::CriterioDeExito
+antropomorfico unAnimal = sabeHablar unAnimal && compararCoeficiente 60 unAnimal
 
---antropomorfico::CriterioDeExito
-antropomorfico unAnimal = any (=="Hablar") (capacidades unAnimal) && (coeficienteIntelectual unAnimal) > 60
+sabeHablar::Animal->Bool
+sabeHablar unAnimal = any (=="Hablar") (capacidades unAnimal)
 
---noTanCuerdo::CriterioDeExito
---noTanCuerdo unAnimal = (length.(filter pinkiesco) (capacidades unAnimal)) > 2
+noTanCuerdo::CriterioDeExito
+noTanCuerdo unAnimal = (>2).length.(filter pinkiesco) (capacidades unAnimal)
 
 --PUNTO 4
-data Experimento = Experimento{listaDeTransformaciones::[Transformacion], criterioDeExito::CriterioDeExito}
+data Experimento = Experimento{transformaciones::[String],criterioDeExito::CriterioDeExito}
 
---experimentoExitoso:: Experimento -> Transformacion
-experimentoExitoso unExperimento unAnimal = all (==True) (map (criterioDeExito unExperimento) (aplicandoTransformaciones unExperimento) unAnimal)
+--experimentoExitoso experimento unAnimal = (criterioDeExito experimento) (aplicarTransformaciones unAnimal transformaciones)
 
---aplicandoTransformaciones::Experimento->Transformacion
-aplicandoTransformaciones unExperimento unAnimal= map (transformar unAnimal) (listaDeTransformaciones unExperimento)
+--aplicarTransformaciones unAnimal transformaciones = map (aplicarTransformacion unAnimal) (transformaciones experimento)
 
---transformar:: Transformacion
-transformar unAnimal unaTransformacion = unaTransformacion unAnimal
+--aplicarTransformacion unAnimal transformacion = transformacion unAnimal
+
+--Necesito una función que hicimos llamada pam que es como map pero le aplica una lista de cosas a un único elemento.
+
+
+
+--"En un ratón de coeficiente intelectual 17, con habilidades de destruenglonir el mundo y hacer planes desalmados,
+-- hacer un experimento que consista en pinkificarlo, luego darle inteligencia superior de 10 y por último darle superpoderes.
+-- Como criterio de éxito, ver si quedó antropomórfico" 
+
+raton = Animal{especie = "Raton", coeficienteIntelectual=17, capacidades=["Destruenglonir el mundo","Hacer planes desalmados"]}
+
+consulta:: CriterioDeExito
+consulta unAnimal = antropomorfico (hacerExperimento unAnimal)
+
+hacerExperimento :: Transformacion
+hacerExperimento = superpoderes.(inteligenciaSuperior 10).pinkificar
+
+--PUNTO 5
+type Reporte = (Ord a) =>[Animal]->[String]->[String]->[a] --Esta firma es lo que pide el ejercicio?
