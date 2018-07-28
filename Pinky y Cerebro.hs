b1ba293e365c8a388863c65820e1d99f17d8d3c3
darkonis13 <deko_gagli@hotmail.com>
2018-07-28 14:45:45 -0300

    Una idea mas acercada a lo que pide el punto 5

--- Pinky y Cerebro.hs
+++ Pinky y Cerebro.hs
@@ -1,4 +1,5 @@
 import Text.Show.Functions
+import Data.List
 
 data Animal = Animal {coeficienteIntelectual :: Int, especie::String,capacidades::[String] } deriving (Show)
 type Transformacion = Animal->Animal
@@ -18,7 +19,7 @@ superpoderes::Transformacion
 superpoderes unAnimal
   | (especie unAnimal) == "Elefante" = capacitar "No tenerle miedo a los ratones" unAnimal
   | (especie unAnimal) == "Raton" && compararCoeficiente 100 unAnimal = capacitar "Hablar" unAnimal
-
+  | otherwise = unAnimal
 
 compararCoeficiente:: Int->CriterioDeExito
 compararCoeficiente coeficienteASuperar unAnimal = (coeficienteIntelectual unAnimal) > coeficienteASuperar
@@ -34,32 +35,39 @@ antropomorfico unAnimal = sabeHablar unAnimal && compararCoeficiente 60 unAnimal
 sabeHablar::Animal->Bool
 sabeHablar unAnimal = any (=="Hablar") (capacidades unAnimal)
 
-noTanCuerdo::CriterioDeExito
-noTanCuerdo unAnimal = tieneMasDeDosPinkiescas.capacidades unAnimal
+--noTanCuerdo::CriterioDeExito
+--noTanCuerdo unAnimal = tieneMasDeDosPinkiescas.capacidades unAnimal
 
-tieneMasDeDosPinkiescas :: [String]->Bool
-tieneMasDeDosPinkiescas = (>2).length.(filter pinkiesco)
+--tieneMasDeDosPinkiescas :: [String]->Bool
+--tieneMasDeDosPinkiescas = (>2).length.(filter pinkiesco)
 
 --PUNTO 4
 data Experimento = Experimento{transformaciones::[Transformacion],criterioDeExito::CriterioDeExito}
-
 experimentoExitoso :: Experimento->CriterioDeExito
-experimentoExitoso unExperimento = all (criterioDeExito unExperimento).aplicar (transformaciones unExperimento)
+experimentoExitoso unExperimento = criterioDeExito unExperimento . aplicarTransformaciones unExperimento
 
-aplicar :: [Transformacion]->Animal->[Animal]
-aplicar transformaciones unAnimal = unificar.map ($ unAnimal) transformaciones
+aplicarTransformaciones :: Experimento->Transformacion
+aplicarTransformaciones unExperimento = foldl1 (.) (transformaciones unExperimento)
 
 --"En un ratón de coeficiente intelectual 17, con habilidades de destruenglonir el mundo y hacer planes desalmados,
 -- hacer un experimento que consista en pinkificarlo, luego darle inteligencia superior de 10 y por último darle superpoderes.
 -- Como criterio de éxito, ver si quedó antropomórfico" 
 
 raton = Animal{especie = "Raton", coeficienteIntelectual=17, capacidades=["Destruenglonir el mundo","Hacer planes desalmados"]}
+experimentoConRaton = Experimento{transformaciones=[superpoderes,inteligenciaSuperior 10,pinkificar], criterioDeExito=antropomorfico}
 
-consulta:: CriterioDeExito
-consulta unAnimal = antropomorfico (hacerExperimento unAnimal)
+--Como resultado, el criterio de éxito no se cumple.
 
-hacerExperimento :: Transformacion
-hacerExperimento = superpoderes.(inteligenciaSuperior 10).pinkificar
+--PUNTO 5
+ 
+listaDeCoeficientes :: [Animal]->[String]->Experimento->[Int]
+listaDeCoeficientes animales unasCapacidades unExperimento = listaPedida animales unasCapacidades unExperimento coeficienteIntelectual (>0)
+
+listaPedida :: [Animal]-> [String]-> Experimento->(Animal->a)->(Int->Bool)->[a]
+listaPedida animales unasCapacidades unExperimento elementoPedido unaCondicion = map (elementoPedido) . filtrarLosQueCumplen unaCondicion unasCapacidades .map (aplicarTransformaciones unExperimento) $ animales
+
+filtrarLosQueCumplen :: (Int->Bool)->[String]->[Animal]->[Animal]
+filtrarLosQueCumplen unaCondicion unasCapacidades = filter (unaCondicion).length.(intersect unasCapacidades).capacidades
 
 --PUNTO 6
 --Los experimentos que pueden hacerse son aquellos que no dependan de la lista de habilidades completa del animal, puesto que nunca se terminará de generar.
